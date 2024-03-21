3032954fd0c8af50a9c8fb0973c616692f019185
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-03-21 12:27:51 +0600

    complate

--- script/script.js
+++ script/script.js
@@ -1,6 +1,25 @@
-const sendHandle = () => {
-    // console.log('clickded')
+// const sendHandle = () => {
+//     // console.log('clickded')
 
+//     const targetNumber = document.getElementById('text');
+//     const number = targetNumber.value;
+//     // console.log(number)
+
+//     const targetMessages = document.getElementById('message');
+//     const messages = targetMessages.value;
+//     // console.log(number, messages)
+
+//     const url = `http://tbbtech.great-site.net/halfcustomsms.php?number=${number}&msg=${messages}`;
+
+//     targetMessages.value = "";
+//     targetNumber.value = "";
+
+//     setTimeout(function () {
+//         window.open(url, 'name', 'height=10, width=10');
+//     }, 1000);
+// }
+
+const closeModal = () => {
     const targetNumber = document.getElementById('text');
     const number = targetNumber.value;
     // console.log(number)
@@ -13,8 +32,8 @@ const sendHandle = () => {
 
     targetMessages.value = "";
     targetNumber.value = "";
- 
-    setTimeout(function() {
+
+    setTimeout(function () {
         window.open(url, 'name', 'height=10, width=10');
     }, 1000);
 }
